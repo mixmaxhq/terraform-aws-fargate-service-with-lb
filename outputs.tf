ce246c4a93ccc3d2475787c73aeac75ef0ea58ba
Ryan Tasson <ryant@mixmax.com>
2020-03-27 10:33:42 -0400

    fix: emit LB/TG ARN suffixes for cloudwatch scaling

--- outputs.tf
+++ outputs.tf
@@ -27,3 +27,14 @@ output "cloudwatch_log_group_name" {
   description = "The name of the CloudWatch log group"
   value       = module.fargate_service.cloudwatch_log_group_name
 }
+
+output "lb_arn_suffix" {
+  description = "The ARN suffix of the application load balancer. Useful for Cloudwatch alarms"
+  value       = module.alb.this_lb_arn_suffix
+}
+
+# As of now, this module only creates one target group; thus we only emit the first item of the list.
+output "tg_arn_suffix" {
+  description = "The ARN suffixes of the target group in the application load balancer. Useful for Cloudwatch alarms"
+  value       = module.alb.target_group_arn_suffixes[0]
+}
