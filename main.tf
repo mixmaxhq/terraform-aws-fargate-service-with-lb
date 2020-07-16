50ca411554b302d99b3e2165e9803d3152454789
Ryan Tasson <ryant@mixmax.com>
2020-07-16 08:21:57 -0400

    fix: bump underlying module for upgraded platform version

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "fargate_service" {
-  source = "git::ssh://git@github.com/mixmaxhq/terraform-aws-fargate-service.git?ref=v2.1.2"
+  source = "git::ssh://git@github.com/mixmaxhq/terraform-aws-fargate-service.git?ref=v2.1.3"
 
   name            = var.name
   environment     = var.environment
